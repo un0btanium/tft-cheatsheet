bd83e4dbe49cbd303bab8af80a4129fd4d0c9b32
un0btanium <unobtaniumlol@gmail.com>
2019-07-22 01:11:28 +0200

    Updates

---
+++ precache-manifest.ade734a2978fb50dfed471831435e0a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de087deb0fa413a2598889587a32ff6",
+    "url": "/tft-cheatsheet/index.html"
+  },
+  {
+    "revision": "79f3e3470de6a0e7a338",
+    "url": "/tft-cheatsheet/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "300ab94e9b1b877abd45",
+    "url": "/tft-cheatsheet/static/css/main.369db9f4.chunk.css"
+  },
+  {
+    "revision": "79f3e3470de6a0e7a338",
+    "url": "/tft-cheatsheet/static/js/2.32a60ece.chunk.js"
+  },
+  {
+    "revision": "300ab94e9b1b877abd45",
+    "url": "/tft-cheatsheet/static/js/main.0b713125.chunk.js"
+  },
+  {
+    "revision": "396c5233f94e82f27660",
+    "url": "/tft-cheatsheet/static/js/runtime~main.a3a98bb4.js"
+  },
+  {
+    "revision": "b6bca47c5e4e7100477fbbfef440e973",
+    "url": "/tft-cheatsheet/static/media/ionicspark.b6bca47c.png"
+  },
+  {
+    "revision": "b36011b8e3794ef17bb733ea112ffe9c",
+    "url": "/tft-cheatsheet/static/media/spatula.b36011b8.png"
+  },
+  {
+    "revision": "1c7e241442e7892f62d96ec269644bf7",
+    "url": "/tft-cheatsheet/static/media/zekesherald.1c7e2414.png"
+  }
+]);
